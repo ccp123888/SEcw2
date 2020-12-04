0704e6dc6e62553317c11b3067de5e07dce3fc65
Nikawlos <cdsl123123123@aliyun.com>
2020-12-04 10:23:04 +0800

    v0.2

---
+++ cw2code ver 0.2/app.py
@@ -0,0 +1,10 @@
+
+def hello():
+    print("Hello world\n")
+    return 1
+
+temp = hello()
+print(temp)
+x = (temp + 1)**2
+y = x + temp
+print(y)
---
+++ cw2code ver 0.2/module.py
@@ -0,0 +1,7 @@
+def bye():
+    print("See u later!")
+
+def add(a,b):
+    c = a + b
+    print("%d + %d = %d" % *(a,b,c))
+    return c
